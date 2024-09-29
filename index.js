b5936a712e0e78aadd752a16cc7513661b758737
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2024-09-29 17:01:12 +0300

    верстка

---
+++ index.js
@@ -0,0 +1,13 @@
+const swiperOne = new Swiper(".swiper-one", {
+  loop: true,
+  slidesPerView: 4.7,
+  spaceBetween: 20,
+  grabCursor: true,
+});
+
+const swiperTwo = new Swiper(".swiper-two", {
+  loop: true,
+  slidesPerView: 4.9,
+  spaceBetween: 20,
+  grabCursor: true,
+});
